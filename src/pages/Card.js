2e5872e623922b3073ab6379d041c89daed52b90
Sebastián Díaz <sebastian88@gmail.com>
2021-04-04 23:29:41 -0400

    null image fix

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,7 +1,7 @@
 const Card = (producto) => {
   const view = `
   <div class="cards__card">
-  <div class="cards__image"><img src="${producto.url_image}" alt="${producto.name}"></div>
+  <div class="cards__image"><img src="${producto.url_image || 'https://upload.wikimedia.org/wikipedia/commons/d/da/Imagen_no_disponible.svg'}" alt="${producto.name}"></div>
   <div class="cards__title">${producto.name}</div>
   <div class="cards__price">$ ${producto.price} ${producto.discount > 0 ? `<span class="cards__discount"> Oferta -${producto.discount}%<span>` : ''}</div>
   <a class="cards__add" href="#"> <i class="cards__addicon fas fa-cart-plus"></i>  </a> 
